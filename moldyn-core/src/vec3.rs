8623f6b3f86bedb3f0fa0251c92f2d6756f6b9d2
Anatoly Wein <anatoly.wein@gmail.com>
2026-04-21 17:56:42 +0200

    add halleys comet and vec3 deserialization ideas

--- moldyn-core/src/vec3.rs
+++ moldyn-core/src/vec3.rs
@@ -4,10 +4,10 @@
 //! See the C++ implementation of the Vec3 class for comparison:
 //! https://github.com/Anatoly03/MolSim-WS25-GroupA/blob/assignment5-local-backup/src/core/math/Vec3.h
 
-use serde::{Deserialize, Serialize};
-use std::ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Neg, Sub, SubAssign};
+use serde::{Deserialize, Serialize, de::Visitor};
+use std::{marker::PhantomData, ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Neg, Sub, SubAssign}};
 
-#[derive(Debug, PartialEq, Clone, Copy, Serialize, Deserialize, Default)]
+#[derive(Debug, PartialEq, Clone, Copy, Serialize, Default)]
 pub struct Vec3<T = f64> {
     pub x: T,
     pub y: T,
@@ -48,6 +48,10 @@ impl<T> Vec3<T> {
         }
     }
 }
+
+//
+// THE FOLLOWING ARE TRAIT IMPLEMENTATIONS FOR THE [Vec3] STRUCT
+//
 
 // vector negation
 impl<T: Neg<Output = T>> Neg for Vec3<T> {
@@ -180,7 +184,7 @@ impl<T: Div<Output = T> + Copy> Div<T> for Vec3<T> {
     /// by a scalar.
     ///
     /// # Example
-    /// 
+    ///
     //// ```rust
     /// let v: Vec3<f64> = Vec3::new(2.0, 4.0, 6.0);
     /// let w: Vec3<f64> = Vec3::new(1.0, 2.0, 3.0);
@@ -206,17 +210,116 @@ impl<T: DivAssign + Copy> DivAssign<T> for Vec3<T> {
     }
 }
 
+//
+// THE FOLLOWING IMPLEMENTS DESERIALIZATION FOR THE [Vec3] STRUCT
+// FROM A THREE-ELEMENT SEQUENCE AND THE X-Y-Z OBJECT
+//
+
+// TODO test (i love my code after hyperfixations, it looks so gloriously clean, but it exhausts my ability to test)
+// see serde deserialization: https://serde.rs/impl-deserialize.html
+
+// We need phantom data because of generics.
+// see https://doc.rust-lang.org/stable/nomicon/phantom-data.html
+struct Vec3Visitor<T>(PhantomData<T>);
+
+impl<'de, T> Visitor<'de> for Vec3Visitor<T>
+where
+    T: Deserialize<'de> + Copy + Default,
+{
+    type Value = Vec3<T>;
+
+    // Format a message stating what data this Visitor expects to receive.
+    fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+        formatter.write_str("3-element array [x, y, z] or object {x, y, z}")
+    }
+
+    /// We deserialize three-element sequences into [Vec3] instances by mapping
+    /// the first element to x, the second to y, and the third to z. Missing values
+    /// are treated as zeroes.
+    fn visit_seq<A>(self, mut seq: A) -> Result<Self::Value, A::Error>
+    where
+        A: serde::de::SeqAccess<'de>,
+    {
+        let x = match seq.next_element() {
+            // [x, ...]
+            Ok(Some(value)) => value,
+            // []
+            Ok(None) => return Ok(Vec3::default()),
+            Err(e) => return Err(e),
+        };
+
+        let y = match seq.next_element() {
+            // [x, y, ...]
+            Ok(Some(value)) => value,
+            // [x]
+            Ok(None) => return Ok(Vec3 { x, ..Default::default() }),
+            Err(e) => return Err(e),
+        };
+
+        let z = match seq.next_element() {
+            // [x, y, z, ...]
+            Ok(Some(value)) => value,
+            // [x, y]
+            Ok(None) => return Ok(Vec3 { x, y, ..Default::default() }),
+            Err(e) => return Err(e),
+        };
+
+        Ok(Vec3 { x, y, z })
+    }
+
+    /// We deserialize maps into [Vec3] instances by looking for keys "x", "y",
+    /// and "z" respectively. If any of these keys are missing, default to zero.
+    fn visit_map<A>(self, mut map: A) -> Result<Self::Value, A::Error>
+    where
+        A: serde::de::MapAccess<'de>,
+    {
+        let mut x = None;
+        let mut y = None;
+        let mut z = None;
+
+        while let Some(key) = map.next_key::<String>()? {
+            match key.as_str() {
+                "x" => x = Some(map.next_value()?),
+                "y" => y = Some(map.next_value()?),
+                "z" => z = Some(map.next_value()?),
+                _ => {
+                    let _: serde::de::IgnoredAny = map.next_value()?;
+                }
+            }
+        }
+
+        // default to zero
+        let x = x.unwrap_or(T::default());
+        let y = y.unwrap_or(T::default());
+        let z = z.unwrap_or(T::default());
+
+        Ok(Vec3 { x, y, z })
+    }
+}
+
+impl<'de, T: Deserialize<'de>> Deserialize<'de> for Vec3<T>
+where
+    T: Deserialize<'de> + Copy + Default,
+{
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        deserializer.deserialize_any(Vec3Visitor(PhantomData))
+    }
+}
+
 macro_rules! impl_for_primitives {
     ($($t:ty),*) => {
 $(impl Vec3<$t> {
     /// Creates the dot product of two [Vec3] instances of the same primitive type.
-    /// 
+    ///
     /// # Example
-    /// 
+    ///
     /// ```rust
     /// let v = Vec3::new(1.0, 2.0, 3.0);
     /// let w = Vec3::new(4.0, 5.0, 6.0);
-    /// 
+    ///
     /// // TODO think about example output
     /// ```
     pub fn dot(&self, other: &Self) -> $t {
@@ -224,13 +327,13 @@ $(impl Vec3<$t> {
     }
 
     /// Creates the cross product of two [Vec3] instances of the same primitive type.
-    /// 
+    ///
     /// # Example
-    /// 
+    ///
     /// ```rust
     /// let v = Vec3::new(1.0, 2.0, 3.0);
     /// let w = Vec3::new(4.0, 5.0, 6.0);
-    /// 
+    ///
     /// // TODO think about example output
     /// ```
     pub fn cross(&self, other: &Self) -> Self {
@@ -242,12 +345,12 @@ $(impl Vec3<$t> {
     }
 
     /// Computes the length (magnitude) of the [Vec3] instance.
-    /// 
+    ///
     /// # Example
-    /// 
+    ///
     /// ```rust
     /// let v = Vec3::new(2.0, 2.0, 1.0);
-    /// 
+    ///
     /// assert_eq!(v.length(), 3.0);
     /// ```
     pub fn length(&self) -> f64 {
@@ -257,7 +360,6 @@ $(impl Vec3<$t> {
 
     /// TODO: rethink: old codebase became buggy because we used the wrong method.
     pub fn length2(&self) -> $t {
-        // https://stackoverflow.com/a/29864160
         self.dot(self)
     }
 
@@ -265,11 +367,12 @@ $(impl Vec3<$t> {
     /// returning an option containing a new [Vec3] instance. If
     /// the original vector is zero-length, returns None to avoid
     /// division by zero.
-    /// 
+    ///
     /// **Note: I've made this decision with the intention of
     /// propagating divisions by zero upwards the stack. I do
     /// not know if this will turn out to be useful.**
     pub fn normal(&self) -> Option<Self> {
+        // divide by zero actually panics: https://internals.rust-lang.org/t/question-why-does-dividing-by-zero-have-no-safety-guarantees/19189
         match self.length() {
             0.0 => None,
             len => Some(Self {
@@ -284,7 +387,5 @@ $(impl Vec3<$t> {
 }
 
 impl_for_primitives!(
-    u8, u16, u32, u64, u128, usize,
-    i8, i16, i32, i64, i128, isize,
-    f32, f64
+    u8, u16, u32, u64, u128, usize, i8, i16, i32, i64, i128, isize, f32, f64
 );
