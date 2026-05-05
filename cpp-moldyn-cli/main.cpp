0cd23acde8a82e3296951edafd73d962a532ba0c
Anatoly Wein <anatoly.wein@gmail.com>
2026-05-05 21:26:45 +0200

    add cli output argument

--- cpp-moldyn-cli/main.cpp
+++ cpp-moldyn-cli/main.cpp
@@ -14,15 +14,17 @@
 int main(int argc, char *argv[])
 {
     std::string input_file_path;
+    std::string output_directory = "./output";
 
     Args()
         .required_details(&input_file_path, "The input file for the simulation. The parser will be selected from the file extension.")
+        .required_details('o', "output", &output_directory, "The output directory for the simulation results.")
         .help("Molecular Dynamics Thesis Code. This library implements a simple engine to simulate molecular dynamics.")
         .version()
         .parse(argc, argv);
 
-
-    std::cout << "Hello, C++!\n";
+    std::cout << "Input file: " << input_file_path << std::endl;
+    std::cout << "Output directory: " << output_directory << std::endl;
 
     return 0;
 }
