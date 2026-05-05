27255892b302f7804884901513890a3db43a7d68
Anatoly Wein <anatoly.wein@gmail.com>
2026-05-05 21:41:02 +0200

    cpp cli retrieve output metadata

--- cpp-moldyn-cli/main.cpp
+++ cpp-moldyn-cli/main.cpp
@@ -7,6 +7,7 @@
 #include <memory>
 
 #include "Args.h"
+#include <filesystem>
 
 /**
  * @brief Entry point for the application.
@@ -14,17 +15,19 @@
 int main(int argc, char *argv[])
 {
     std::string input_file_path;
-    std::string output_directory = "./output";
+    std::filesystem::path output_pattern = "./output/out.vtk";
 
     Args()
         .required_details(&input_file_path, "The input file for the simulation. The parser will be selected from the file extension.")
-        .required_details('o', "output", &output_directory, "The output directory for the simulation results.")
+        .required_details('o', "output", &output_pattern, "The output directory for the simulation results.")
         .help("Molecular Dynamics Thesis Code. This library implements a simple engine to simulate molecular dynamics.")
         .version()
         .parse(argc, argv);
 
-    std::cout << "Input file: " << input_file_path << std::endl;
-    std::cout << "Output directory: " << output_directory << std::endl;
+    printf("Input file: %s\n", input_file_path.c_str());
+    printf("Output directory: %s\n", output_pattern.parent_path().string().c_str());
+    printf("Output prefix: %s\n", output_pattern.stem().string().c_str());
+    printf("Output extension: %s\n", output_pattern.extension().string().c_str());
 
     return 0;
 }
