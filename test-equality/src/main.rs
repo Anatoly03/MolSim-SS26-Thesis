1d86382a2a3c65fa16248be79ed2bed5ab829061
Anatoly Wein <anatoly.wein@gmail.com>
2026-07-27 09:12:37 +0200

    move some benchmarks to extended feature

--- test-equality/src/main.rs
+++ test-equality/src/main.rs
@@ -46,27 +46,48 @@ fn main() {
     Log::Info.log("Stepping\t", &cpu.stepping.to_string());
     Log::Info.log("Boost enabled\t", &cpu.boost_enabled);
 
-    // this benchmark verifies halleys comet correctness over 25 thousand steps
-    // cpp::run("halleys-comet", 0.0014, 25000);
-    // rust::run("halleys-comet", 0.0014, 25000);
-    // test::run("halleys-comet", 25000);
-    cpp::bench("halleys-comet", 0.0014, 25000000);
-    rust::bench("halleys-comet", 0.0014, 25000000);
-
-    // this benchmark does nothing useful
-    cpp::run("two-colliding-particles", 0.0014, 100);
-    rust::run("two-colliding-particles", 0.0014, 100);
-    test::run("two-colliding-particles", 100);
+    #[cfg(feature = "extended")]
+    {   
+        // this benchmark verifies halleys comet correctness over 25 thousand steps
+        // cpp::run("halleys-comet", 0.0014, 25000);
+        // rust::run("halleys-comet", 0.0014, 25000);
+        // test::run("halleys-comet", 25000);
+        cpp::bench("halleys-comet", 0.0014, 25000000);
+        rust::bench("halleys-comet", 0.0014, 25000000);
+    
+        // this benchmark does nothing useful
+        cpp::run("two-colliding-particles", 0.0014, 100);
+        rust::run("two-colliding-particles", 0.0014, 100);
+        test::run("two-colliding-particles", 100);
+    }
 
     // this benchmark measures I/O performance
     cpp::run("two-bodies-collision-0001", 0.0007, 1);
     rust::run("two-bodies-collision-0001", 0.0007, 1);
     test::run("two-bodies-collision-0001", 1);
 
+    #[cfg(feature = "full")]
+    {
+        cpp::bench("two-bodies-collision-0001", 0.0007, 10);
+        rust::bench("two-bodies-collision-0001", 0.0007, 10);
+    }
+
     // this benchmark additionally measures the accumulation of floating-point errors
     // over many steps
     cpp::bench("two-bodies-collision-0001", 0.0007, 20);
     rust::bench("two-bodies-collision-0001", 0.0007, 20);
+
+    #[cfg(feature = "full")]
+    {
+        cpp::bench("two-bodies-collision-0001", 0.0007, 30);
+        rust::bench("two-bodies-collision-0001", 0.0007, 30);
+    }
+
+    #[cfg(feature = "full")]
+    {
+        cpp::bench("two-bodies-collision-0001", 0.0007, 40);
+        rust::bench("two-bodies-collision-0001", 0.0007, 40);
+    }
 
     // this benchmark additionally measures the accumulation of floating-point errors
     // over many steps
@@ -78,10 +99,25 @@ fn main() {
     rust::run("two-bodies-collision-0001-linked-cells", 0.0007, 1);
     test::run("two-bodies-collision-0001-linked-cells", 1);
 
+    #[cfg(feature = "full")]
+    {
+        cpp::bench("two-bodies-collision-0001-linked-cells", 0.0007, 10);
+        rust::bench("two-bodies-collision-0001-linked-cells", 0.0007, 10);
+    }
+
     // this benchmark additionally measures the accumulation of floating-point errors
     // over many steps
     cpp::bench("two-bodies-collision-0001-linked-cells", 0.0007, 20);
     rust::bench("two-bodies-collision-0001-linked-cells", 0.0007, 20);
+
+    #[cfg(feature = "full")]
+    {
+        cpp::bench("two-bodies-collision-0001-linked-cells", 0.0007, 30);
+        rust::bench("two-bodies-collision-0001-linked-cells", 0.0007, 30);
+
+        cpp::bench("two-bodies-collision-0001-linked-cells", 0.0007, 40);
+        rust::bench("two-bodies-collision-0001-linked-cells", 0.0007, 40);
+    }
 
     // this benchmark additionally measures the accumulation of floating-point errors
     // over many steps
@@ -92,8 +128,23 @@ fn main() {
     cpp::run("two-bodies-collision-0001-parallel", 0.0007, 1);
     rust::run("two-bodies-collision-0001-parallel", 0.0007, 1);
 
+    #[cfg(feature = "full")]
+    {
+        cpp::bench("two-bodies-collision-0001-parallel", 0.0007, 10);
+        rust::bench("two-bodies-collision-0001-parallel", 0.0007, 10);
+    }
+
     cpp::bench("two-bodies-collision-0001-parallel", 0.0007, 20);
     rust::bench("two-bodies-collision-0001-parallel", 0.0007, 20);
+
+    #[cfg(feature = "full")]
+    {
+        cpp::bench("two-bodies-collision-0001-parallel", 0.0007, 30);
+        rust::bench("two-bodies-collision-0001-parallel", 0.0007, 30);
+
+        cpp::bench("two-bodies-collision-0001-parallel", 0.0007, 40);
+        rust::bench("two-bodies-collision-0001-parallel", 0.0007, 40);
+    }
 
     cpp::bench("two-bodies-collision-0001-parallel", 0.0007, 50);
     rust::bench("two-bodies-collision-0001-parallel", 0.0007, 50);
