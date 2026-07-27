c812b9f641bea784db16bfade41e996d8990b9e8
Anatoly Wein <anatoly.wein@gmail.com>
2026-07-27 09:55:09 +0200

    refactor bbenchmark script

--- test-equality/src/main.rs
+++ test-equality/src/main.rs
@@ -21,6 +21,30 @@ pub const REPETITIONS: usize = 5;
 #[cfg(feature = "full")]
 pub const REPETITIONS: usize = 20;
 
+/// Maximal amount of ticks (repetitions) to run a program for benching. It steps
+/// every `10` timesteps and was previously set to `50` for CI. With the `full`
+/// feature it is extended to make sure the Two Bodies Collision occurs for linked
+/// cells.
+#[cfg(not(feature = "full"))]
+#[allow(non_snake_case)]
+pub fn TIME_STEPS() -> Vec<usize> {
+    return vec![1, 20, 50];
+}
+
+/// Maximal amount of ticks (repetitions) to run a program for benching. It steps
+/// every `10` timesteps and was previously set to `50` for CI. With the `full`
+/// feature it is extended to make sure the Two Bodies Collision occurs for linked
+/// cells.
+#[cfg(feature = "full")]
+pub fn TIME_STEPS() -> Vec<usize> {
+    use std::ops::Range;
+
+    Range { start: 10, end: 500 }
+        .into_iter()
+        .filter(|i| i % 10 == 0)
+        .collect::<Vec<usize>>()
+}
+
 fn main() {
     cpp::build();
     rust::build();
@@ -47,14 +71,14 @@ fn main() {
     Log::Info.log("Boost enabled\t", &cpu.boost_enabled);
 
     #[cfg(feature = "extended")]
-    {   
+    {
         // this benchmark verifies halleys comet correctness over 25 thousand steps
         // cpp::run("halleys-comet", 0.0014, 25000);
         // rust::run("halleys-comet", 0.0014, 25000);
         // test::run("halleys-comet", 25000);
         cpp::bench("halleys-comet", 0.0014, 25000000);
         rust::bench("halleys-comet", 0.0014, 25000000);
-    
+
         // this benchmark does nothing useful
         cpp::run("two-colliding-particles", 0.0014, 100);
         rust::run("two-colliding-particles", 0.0014, 100);
@@ -66,86 +90,27 @@ fn main() {
     rust::run("two-bodies-collision-0001", 0.0007, 1);
     test::run("two-bodies-collision-0001", 1);
 
-    #[cfg(feature = "full")]
-    {
-        cpp::bench("two-bodies-collision-0001", 0.0007, 10);
-        rust::bench("two-bodies-collision-0001", 0.0007, 10);
+    for frames in TIME_STEPS() {
+        cpp::bench("two-bodies-collision-0001", 0.0007, frames);
+        rust::bench("two-bodies-collision-0001", 0.0007, frames);
     }
-
-    // this benchmark additionally measures the accumulation of floating-point errors
-    // over many steps
-    cpp::bench("two-bodies-collision-0001", 0.0007, 20);
-    rust::bench("two-bodies-collision-0001", 0.0007, 20);
-
-    #[cfg(feature = "full")]
-    {
-        cpp::bench("two-bodies-collision-0001", 0.0007, 30);
-        rust::bench("two-bodies-collision-0001", 0.0007, 30);
-    }
-
-    #[cfg(feature = "full")]
-    {
-        cpp::bench("two-bodies-collision-0001", 0.0007, 40);
-        rust::bench("two-bodies-collision-0001", 0.0007, 40);
-    }
-
-    // this benchmark additionally measures the accumulation of floating-point errors
-    // over many steps
-    cpp::bench("two-bodies-collision-0001", 0.0007, 50);
-    rust::bench("two-bodies-collision-0001", 0.0007, 50);
 
     // this benchmark measures I/O performance
     cpp::run("two-bodies-collision-0001-linked-cells", 0.0007, 1);
     rust::run("two-bodies-collision-0001-linked-cells", 0.0007, 1);
     test::run("two-bodies-collision-0001-linked-cells", 1);
 
-    #[cfg(feature = "full")]
-    {
-        cpp::bench("two-bodies-collision-0001-linked-cells", 0.0007, 10);
-        rust::bench("two-bodies-collision-0001-linked-cells", 0.0007, 10);
+    for frames in TIME_STEPS() {
+        cpp::bench("two-bodies-collision-0001-linked-cells", 0.0007, frames);
+        rust::bench("two-bodies-collision-0001-linked-cells", 0.0007, frames);
     }
-
-    // this benchmark additionally measures the accumulation of floating-point errors
-    // over many steps
-    cpp::bench("two-bodies-collision-0001-linked-cells", 0.0007, 20);
-    rust::bench("two-bodies-collision-0001-linked-cells", 0.0007, 20);
-
-    #[cfg(feature = "full")]
-    {
-        cpp::bench("two-bodies-collision-0001-linked-cells", 0.0007, 30);
-        rust::bench("two-bodies-collision-0001-linked-cells", 0.0007, 30);
-
-        cpp::bench("two-bodies-collision-0001-linked-cells", 0.0007, 40);
-        rust::bench("two-bodies-collision-0001-linked-cells", 0.0007, 40);
-    }
-
-    // this benchmark additionally measures the accumulation of floating-point errors
-    // over many steps
-    cpp::bench("two-bodies-collision-0001-linked-cells", 0.0007, 50);
-    rust::bench("two-bodies-collision-0001-linked-cells", 0.0007, 50);
 
     // this benchmark measures I/O performance
     cpp::run("two-bodies-collision-0001-parallel", 0.0007, 1);
     rust::run("two-bodies-collision-0001-parallel", 0.0007, 1);
 
-    #[cfg(feature = "full")]
-    {
-        cpp::bench("two-bodies-collision-0001-parallel", 0.0007, 10);
-        rust::bench("two-bodies-collision-0001-parallel", 0.0007, 10);
+    for frames in TIME_STEPS() {
+        cpp::bench("two-bodies-collision-0001-parallel", 0.0007, frames);
+        rust::bench("two-bodies-collision-0001-parallel", 0.0007, frames);
     }
-
-    cpp::bench("two-bodies-collision-0001-parallel", 0.0007, 20);
-    rust::bench("two-bodies-collision-0001-parallel", 0.0007, 20);
-
-    #[cfg(feature = "full")]
-    {
-        cpp::bench("two-bodies-collision-0001-parallel", 0.0007, 30);
-        rust::bench("two-bodies-collision-0001-parallel", 0.0007, 30);
-
-        cpp::bench("two-bodies-collision-0001-parallel", 0.0007, 40);
-        rust::bench("two-bodies-collision-0001-parallel", 0.0007, 40);
-    }
-
-    cpp::bench("two-bodies-collision-0001-parallel", 0.0007, 50);
-    rust::bench("two-bodies-collision-0001-parallel", 0.0007, 50);
 }
